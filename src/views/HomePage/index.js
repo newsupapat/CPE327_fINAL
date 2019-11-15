842be22fd4b187e3a4d5af31a3b986371abe5414
Kuttsuki <45891652+Kuttsuki@users.noreply.github.com>
2019-11-15 21:47:46 +0700

    Edit homepage
    -profile pict
    -progress
    -menu

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Navbar from 'components/Navbars';
-import { Input, Menu, Segment,List,Image } from 'semantic-ui-react';
+import { Input, Menu, Segment,List,Image,Grid,Progress } from 'semantic-ui-react';
 import './home.css'
 import logo from 'asset/image/logo.png'
+import profile from 'asset/image/logo512.png'
 
 export default class MenuExampleTabularOnTop extends React.Component {
   state = { activeItem: 'bio' }
@@ -23,7 +24,13 @@ export default class MenuExampleTabularOnTop extends React.Component {
     const { activeItem } = this.state
 
     return (
+      
       <Navbar>
+        <div className="profile">
+        <Progress percent={33} size={"large"} style={{marginBottom: "0px"}} />
+        <Image src={profile} size='medium' circular />
+        <Progress percent={33} size={"large"} style={{marginBottom: "0px"}} />
+        </div>
         <Menu attached='top' tabular>
           <Menu.Item
             name='bio'
