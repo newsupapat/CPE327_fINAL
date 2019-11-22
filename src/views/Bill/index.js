94f3a4077d9cc07568269957e4ec529f3540443c
shiroikatsu <34379387+shiroikatsu@users.noreply.github.com>
2019-11-22 16:00:29 +0700

    card in add bill

--- src/views/Bill/index.js
+++ src/views/Bill/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import Navbar from 'components/Navbars';
 import { Segment,
         Container,
-        Dropdown } from 'semantic-ui-react'
+        Dropdown,
+        Card,
+        Input,
+        Grid, 
+        GridRow,
+        GridColumn} from 'semantic-ui-react'
 
 
 import './bill.css';
@@ -12,11 +17,18 @@ import './bill.css';
 export default class MenuExampleTabularOnTop extends React.Component {
 
   render() {
-    const options = [
+    const category = [
       { key: 1, text: 'อาหาร', value: 1 },
       { key: 2, text: 'เสื้อผ้า', value: 2 },
       { key: 3, text: 'เดินทาง', value: 3 },
       { key: 4, text: 'อื่นๆ', value: 4 }
+    ]
+
+    const friend = [
+      { key: 1, text: 'kat', value: 1 },
+      { key: 2, text: 'new', value: 2 },
+      { key: 3, text: 'rose', value: 3 },
+      { key: 4, text: 'tangkwa', value: 4 }
     ]
 
     return (
@@ -25,10 +37,38 @@ export default class MenuExampleTabularOnTop extends React.Component {
           <Segment raised>
             <Container>
               <h4>หมวดหมู่</h4>
-              <Dropdown selection options={options} placeholder='เลือกหมวดหมู่' />
+              <Dropdown selection options={category} placeholder='เลือกหมวดหมู่' />
             </Container>
-            <Segment raised>
-            </Segment>
+            <hr style={{ backgroundColor: 'Lightgray', height: '1px' }}></hr>
+            <Card>
+            <Grid columns={2} divided>
+                <GridRow>
+                  <GridColumn>
+                    <label>ชื่อรายการ</label>
+                  </GridColumn>
+                  <GridColumn>
+                    <Input></Input>
+                  </GridColumn>
+                </GridRow>
+                <GridRow>
+                  <GridColumn>
+                    <label>จำนวนเงิน</label>
+                  </GridColumn>
+                  <GridColumn>
+                    <Input></Input>
+                  </GridColumn>
+                </GridRow>
+                <GridRow>
+                  <GridColumn>
+                    <label>เพื่อน</label>
+                  </GridColumn>
+                  <GridColumn>
+                    {/* shit happen here */}
+                    <Dropdown style={{backgroundColor:"white",width : 10,margin : "0px 0px 0px -15px" }} selection options={friend} placeholder='เลือกเพื่อน' />
+                  </GridColumn>
+                </GridRow>
+            </Grid>
+          </Card>
           </Segment>
         </Navbar>
     );
