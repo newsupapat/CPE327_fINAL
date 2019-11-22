c1e2b8a3b32424b2ea2a86e99287b122be31647f
shiroikatsu <34379387+shiroikatsu@users.noreply.github.com>
2019-11-22 14:08:36 +0700

    add bill, segment is too long

--- src/views/Bill/index.js
+++ src/views/Bill/index.js
@@ -1,59 +1,36 @@
 import React from 'react';
 import Navbar from 'components/Navbars';
-import { Header } from 'semantic-ui-react'
+import { Segment,
+        Container,
+        Dropdown } from 'semantic-ui-react'
 
 
-//import './group.css';
+import './bill.css';
 // import Step1 from './segment/step1';
 // import Step2 from './segment/step2';
 // import Step3 from './segment/step3';
 export default class MenuExampleTabularOnTop extends React.Component {
-//   state = {
-//     currentStep: 1,
-//     step1: null,
-//     friend: [],
-//   };
-//   handleSubmitStep1 = (value) => {
-//     this.setState({ step1: value });
-//     this.setState({ currentStep: 2 });
-//   };
-//   handleSubmitStep2 = (value) => {
-//     this.setState({ friend: value });
-//     this.setState({ currentStep: 3 });
-//   };
+
   render() {
+    const options = [
+      { key: 1, text: 'อาหาร', value: 1 },
+      { key: 2, text: 'เสื้อผ้า', value: 2 },
+      { key: 3, text: 'เดินทาง', value: 3 },
+      { key: 4, text: 'อื่นๆ', value: 4 }
+    ]
+
     return (
        <Navbar>
-           <Header as='h1' style={{color:"#fff"}}>สร้างบิล</Header>
+          <h1>สร้างบิล</h1>
+          <Segment raised>
+            <Container>
+              <h4>หมวดหมู่</h4>
+              <Dropdown selection options={options} placeholder='เลือกหมวดหมู่' />
+            </Container>
+            <Segment raised>
+            </Segment>
+          </Segment>
         </Navbar>
-    //     <Step1
-    //       onsubmit={(e) => this.handleSubmitStep1(e)}
-    //       currentstep={this.state.currentStep}
-    //       value={this.state.step1}
-    //     />
-    //     <Step2
-    //       onsubmit={(e) => this.handleSubmitStep2(e)}
-    //       currentstep={this.state.currentStep}
-    //     />
-    //     <Step3
-    //       onsubmit={(e) => this.handleSubmitStep2(e)}
-    //       currentstep={this.state.currentStep}
-    //       value={
-    //         this.state.currentStep !== 3 ? (
-    //           { ...this.state.step1, friend: this.state.friend }
-    //         ) : (
-    //           {
-    //             activeItem: 'tour',
-    //             friend: [
-    //               { id: 1, name: 'new', select: true },
-    //               { id: 2, name: 'rose', select: true },
-    //             ],
-    //             imagePreviewUrl: null,
-    //             name: 'เคเอฟซี',
-    //           }
-    //         )
-    //       }
-    //     />
     );
   }
 }
