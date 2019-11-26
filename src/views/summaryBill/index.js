21c52cd2ce1b9ab784122f8a11df3659486bb835
shiroikatsu <34379387+shiroikatsu@users.noreply.github.com>
2019-11-26 20:51:05 +0700

    newsquaz add modal

--- src/views/summaryBill/index.js
+++ src/views/summaryBill/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from 'components/Navbars';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import {
   Segment,
   Container,
-  Button,
   Image,
   List,
   Label,
@@ -15,6 +15,9 @@ import billsum from './sumbill.css';
 
 const SummaryBill = ({ match }) => {
   const [Owner, setOwner] = useState(null);
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -67,6 +70,17 @@ const SummaryBill = ({ match }) => {
           >
             {Owner && Owner.name}
           </h2>
+          <Button color="danger" onClick={toggle}>new</Button>
+      <Modal isOpen={modal} toggle={toggle} >
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
           {/* <Image
             src={profile}
             size="medium"
